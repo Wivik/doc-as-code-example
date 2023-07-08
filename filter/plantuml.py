661794c108d78c4a9c0f38e4cd43e99513a6aa4f
Wivik <48727868+Wivik@users.noreply.github.com>
2023-07-08 12:10:46 +0200

    fix: removed puml, odt and png files

--- filter/plantuml.py
+++ filter/plantuml.py
@@ -9,14 +9,16 @@ def plantuml(key, value, format, _):
     if key == 'CodeBlock':
         [[ident, classes, keyvals], code] = value
         if "plantuml" in classes:
-            filename = sha1(code.encode(sys.getfilesystemencoding())).hexdigest() + ".png"
-            if not os.path.isfile(filename):
-                with open(filename[:-4], "w") as f:  # create a .uml file
+            base_filename = sha1(code.encode(sys.getfilesystemencoding())).hexdigest()
+            plantuml_filename = base_filename + ".puml"
+            png_filename = base_filename + ".png"
+            if not os.path.isfile(png_filename):
+                with open(plantuml_filename, "w") as f:  # create a .uml file
                     f.write("@startuml\n")
                     f.write(code)
                     f.write("\n@enduml")
-                call(["plantuml", filename[:-4]])  # convert the .uml file into a .png file
-            return Para([Image([ident, [], keyvals], [], [filename, ""])])
+                call(["plantuml", plantuml_filename])  # convert the .uml file into a .png file
+            return Para([Image([ident, [], keyvals], [], [png_filename, ""])])
 
 if __name__ == "__main__":
     toJSONFilter(plantuml)
